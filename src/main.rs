cf0a051f1d6173a220a937ddae26510442164452
weder.sousa <weder96@gmail.com>
2019-08-17 12:59:53 -0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate nickel;
+use nickel::{Nickel, HttpRouter};
+const INDEX: &str = include_str!("index.html");
+
+fn main() {
+    let mut server = Nickel::new();
+    server.get("/", middleware!(INDEX));
+
+    let port = std::env::var("PORT").unwrap_or("3000".into());
+    let url = format!("0.0.0.0:{port}", port = port);
+
+    println!("On {}", url);
+    server.listen(url)
+        .expect("Não conseguimos iniciar o servidor");
+}
